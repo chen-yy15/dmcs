7b1dde280249338273ed03ffcbc43a51aedeb1dd
chenning007 <25713578@qq.com>
2017-06-27 07:31:42 +0800

    user function commit

--- src/main/java/edu/tsinghua/dmcs/web/UserRestController.java
+++ src/main/java/edu/tsinghua/dmcs/web/UserRestController.java
@@ -1,7 +1,10 @@
 package edu.tsinghua.dmcs.web;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.tsinghua.dmcs.entity.User;
@@ -15,41 +18,90 @@ public class UserRestController {
 	@Autowired
 	private UserMapper userMapper;
 	
+	@RequestMapping("checkExistence")
+	public boolean checkExistence(@RequestParam String username) {
+		User user = userMapper.selectByUserName(username);
+		return user != null;
+	}
+	
 	@RequestMapping("/register")
-	public User register() {
+	public User register(@RequestParam String username,
+			@RequestParam String realname,
+			@RequestParam String title,
+			@RequestParam String idcard,
+			@RequestParam String password,
+			@RequestParam String alias,
+			@RequestParam String birthday,
+			@RequestParam String image,
+			@RequestParam String icon,
+			@RequestParam String email,
+			@RequestParam String mobile) {
 		
 		User u = new User();
-		u.setUid("cje");
+		u.setUid("1"); // TODO
+		u.setUsername(username);
+		u.setRealname(realname);
+		u.setTitle(title);
+		u.setIdcard(idcard);
+		u.setPassword(password);// TODO
+		u.setAlias(alias);
+		Date birth = new Date(birthday);
+		u.setBirthday(birth); // TODO
+		u.setImage(image);
+		u.setIcon(icon);
+		u.setEmail(email);
+		u.setMobile(mobile);
+		u.setRegtime(new Date());
 		userMapper.insert(u);
-		// user exists?
-		// ingest user
 		
 		return u;
 		
 	}
 	
 	@RequestMapping("/login")
-	public User login() {
+	public boolean login(@RequestParam String username,
+			@RequestParam String password) {
 		
-		User u = new User();
-		u.setUid("cje");
-		userMapper.insert(u);
-		// user exists?
-		// ingest user
+		User u = userMapper.selectByUserName(username);
+		if(u != null) {
+			if(u.getPassword().equals(password))
+				return true;
+		}
 		
-		return u;
+		return false;
 		
 	}
 	
 	@RequestMapping("/update")
-	public User update() {
+	public User update(@RequestParam String username,
+			@RequestParam String realname,
+			@RequestParam String title,
+			@RequestParam String idcard,
+			@RequestParam String password,
+			@RequestParam String alias,
+			@RequestParam String birthday,
+			@RequestParam String image,
+			@RequestParam String icon,
+			@RequestParam String email,
+			@RequestParam String mobile) {
 		
 		User u = new User();
-		u.setUid("cje");
-		userMapper.insert(u);
-		// user exists?
-		// ingest user
 		
+		u = userMapper.selectByUserName(username);
+		if(u != null) {
+			u.setRealname(realname);
+			u.setTitle(title);
+			u.setIdcard(idcard);
+			u.setPassword(password);// TODO
+			u.setAlias(alias);
+			Date birth = new Date(birthday);
+			u.setBirthday(birth); // TODO
+			u.setImage(image);
+			u.setIcon(icon);
+			u.setEmail(email);
+			u.setMobile(mobile);
+		}
+		userMapper.updateByPrimaryKey(u);
 		return u;
 		
 	}
