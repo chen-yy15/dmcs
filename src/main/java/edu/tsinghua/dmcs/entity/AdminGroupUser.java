9ddcfb0de7daf26a88545ad9631831199c27c315
caizj15 <caizj15@qq.com>
2018-09-06 22:44:59 +0800

    add AdminGroupUser

---
+++ src/main/java/edu/tsinghua/dmcs/entity/AdminGroupUser.java
@@ -0,0 +1,7 @@
+package edu.tsinghua.dmcs.entity;
+
+/**
+ * Created by caizj on 18-9-6.
+ */
+public class AdminGroupUser {
+}
---
+++ src/main/java/edu/tsinghua/dmcs/mapper/AdminGroupUserMapper.java
@@ -0,0 +1,7 @@
+package edu.tsinghua.dmcs.mapper;
+
+/**
+ * Created by caizj on 18-9-6.
+ */
+public interface AdminGroupUserMapper {
+}
