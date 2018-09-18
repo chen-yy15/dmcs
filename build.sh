7173c1d6e826a6809491b53ad38b8e7a3af26499
caizj15 <caizj15@qq.com>
2018-09-18 21:38:45 +0800

    change build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 rm -f nohup.out
+echo "delete nohup.out !!!"
 git pull
 mvn clean package
 nohup java -jar ./target/dmcs.jar &
 echo $! > /var/run/Test.pid
+echo " dmcs.jar is running..."
