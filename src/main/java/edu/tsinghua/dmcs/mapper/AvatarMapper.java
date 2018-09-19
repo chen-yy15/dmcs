acf632f0dd964ab38b3d4aaa8afd800f08acd9e8
caizj15 <caizj15@qq.com>
2018-09-19 22:09:39 +0800

    change AvatarMapper

--- src/main/java/edu/tsinghua/dmcs/mapper/AvatarMapper.java
+++ src/main/java/edu/tsinghua/dmcs/mapper/AvatarMapper.java
@@ -1,7 +1,24 @@
 package edu.tsinghua.dmcs.mapper;
 
+import edu.tsinghua.dmcs.entity.Avatar;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
 /**
  * Created by caizj on 18-9-19.
  */
+@Mapper
 public interface AvatarMapper {
+    int addAvatar(Avatar avatar);
+
+    int deleteById(Long avatarId);
+
+    int updateById(Avatar avatar);
+
+    Avatar selectById(Long avatarId);
+
+    List<Avatar> querySelectByUserid(String userId);
+
+    int queryDeleteByUserid(String userId);
 }
