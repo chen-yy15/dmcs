93423a0fd01125356c54f024852375b0509ca296
caizj15 <caizj15@qq.com>
2018-09-20 11:07:34 +0800

    change AddressService

--- src/main/java/edu/tsinghua/dmcs/service/AddressService.java
+++ src/main/java/edu/tsinghua/dmcs/service/AddressService.java
@@ -1,7 +1,22 @@
 package edu.tsinghua.dmcs.service;
 
+import edu.tsinghua.dmcs.entity.UserAddress;
+
+import java.util.List;
+
 /**
  * Created by caizj on 18-9-20.
  */
 public interface AddressService {
+    public int AddAddress(UserAddress userAddress);
+
+    public int DeleteById(Long addressId);
+
+    public int DeleteByUserId(String userId);
+
+    public int UpdateById(UserAddress userAddress);
+
+    public UserAddress SelectById(Long addressId);
+
+    public List<UserAddress> SelectByUserId(String userId);
 }
--- src/main/java/edu/tsinghua/dmcs/service/impl/AddressServiceImpl.java
+++ src/main/java/edu/tsinghua/dmcs/service/impl/AddressServiceImpl.java
@@ -1,7 +1,52 @@
 package edu.tsinghua.dmcs.service.impl;
 
+import edu.tsinghua.dmcs.entity.UserAddress;
+import edu.tsinghua.dmcs.mapper.UserAddressMapper;
+import edu.tsinghua.dmcs.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
 /**
  * Created by caizj on 18-9-20.
  */
-public class AddressServiceImpl {
+@Component
+public class AddressServiceImpl implements AddressService {
+
+    @Autowired
+    UserAddressMapper userAddressMapper;
+
+
+    public int AddAddress(UserAddress userAddress){
+        int num = 0;
+        if(userAddress!=null){
+            num = userAddressMapper.addAddress(userAddress);
+        }
+        return num;
+    }
+
+    public int DeleteById(Long addressId){
+        return userAddressMapper.deleteById(addressId);
+    }
+
+    public int DeleteByUserId(String userId){
+        return userAddressMapper.deleteByUserId(userId);
+    }
+
+    public int UpdateById(UserAddress userAddress){
+        int num = 0;
+        if(userAddress!=null){
+            num = userAddressMapper.updateById(userAddress);
+        }
+        return num;
+    }
+
+    public UserAddress SelectById(Long addressId){
+        return userAddressMapper.selectById(addressId);
+    }
+
+    public List<UserAddress> SelectByUserId(String userId){
+        return userAddressMapper.querySelectByUserId(userId);
+    }
 }
