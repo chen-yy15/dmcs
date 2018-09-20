63555855a637b5ef8209f644ab72e3371925d7df
caizj15 <caizj15@qq.com>
2018-09-21 01:26:26 +0800

    change LoginLogService

--- src/main/java/edu/tsinghua/dmcs/service/LoginLogService.java
+++ src/main/java/edu/tsinghua/dmcs/service/LoginLogService.java
@@ -1,7 +1,21 @@
-package edu.tsinghua.dmcs.service.impl;
+package edu.tsinghua.dmcs.service;
+
+import edu.tsinghua.dmcs.entity.LoginLog;
+
+import java.util.List;
 
 /**
  * Created by caizj on 18-9-21.
  */
 public interface LoginLogService {
+
+    public int AddLog(LoginLog loginLog);
+
+    public int DeleteLog(Long logid);
+
+    public int DeleteByUserid(String userid);
+
+    public LoginLog GetLog(Long logid);
+
+    public List<LoginLog> QueryLog(String userid);
 }
--- src/main/java/edu/tsinghua/dmcs/service/impl/LoginLogServiceImpl.java
+++ src/main/java/edu/tsinghua/dmcs/service/impl/LoginLogServiceImpl.java
@@ -1,7 +1,47 @@
 package edu.tsinghua.dmcs.service.impl;
 
+import edu.tsinghua.dmcs.entity.LoginLog;
+import edu.tsinghua.dmcs.mapper.LoginLogMapper;
+import edu.tsinghua.dmcs.service.LoginLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
 /**
  * Created by caizj on 18-9-21.
  */
-public class LoginLogServiceImpl {
+@Component
+public class LoginLogServiceImpl implements LoginLogService {
+
+    @Autowired
+    LoginLogMapper loginLogMapper;
+
+    public int AddLog(LoginLog loginLog){
+        int num = 0;
+        if(loginLog!=null){
+            num = loginLogMapper.addLog(loginLog);
+        }
+        return num;
+    }
+
+    public int DeleteLog(Long logid){
+        int num = 0;
+        num = loginLogMapper.deleteLog(logid);
+        return num;
+    }
+
+    public int DeleteByUserid(String userid){
+        int num = 0;
+        num = loginLogMapper.deleteByUserid(userid);
+        return num;
+    }
+
+    public LoginLog GetLog(Long logid){
+        return loginLogMapper.getLog(logid);
+    }
+
+    public List<LoginLog> QueryLog(String userid){
+        return loginLogMapper.queryGetLog(userid);
+    }
 }
