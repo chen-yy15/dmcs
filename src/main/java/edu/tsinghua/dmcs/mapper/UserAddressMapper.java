97cab225916c9cc9ff3ed550e8530071e9ea14ea
caizj15 <caizj15@qq.com>
2018-09-20 11:06:46 +0800

    change userAddressMapper

--- src/main/java/edu/tsinghua/dmcs/mapper/UserAddressMapper.java
+++ src/main/java/edu/tsinghua/dmcs/mapper/UserAddressMapper.java
@@ -1,7 +1,29 @@
 package edu.tsinghua.dmcs.mapper;
 
+import edu.tsinghua.dmcs.entity.UserAddress;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
 /**
  * Created by caizj on 18-9-20.
  */
+
+/****
+ * 把这个数据直接放入到userservice当中去
+ */
+@Mapper
 public interface UserAddressMapper {
+    int addAddress(UserAddress userAddress);
+
+    int deleteById(Long addressId);
+
+    int deleteByUserId(String userId);
+
+    int updateById(UserAddress userAddress);
+
+    UserAddress selectById(Long addressId);
+
+    List<UserAddress> querySelectByUserId(String userId);
+
 }
