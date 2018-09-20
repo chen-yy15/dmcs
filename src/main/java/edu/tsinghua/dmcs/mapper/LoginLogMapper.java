2e8ce09c362f1ce707b7bc1811f22fa715c4d12c
caizj15 <caizj15@qq.com>
2018-09-21 01:25:50 +0800

    change LoginLogMapper

--- src/main/java/edu/tsinghua/dmcs/mapper/LoginLogMapper.java
+++ src/main/java/edu/tsinghua/dmcs/mapper/LoginLogMapper.java
@@ -1,7 +1,23 @@
 package edu.tsinghua.dmcs.mapper;
 
+import edu.tsinghua.dmcs.entity.LoginLog;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
 /**
  * Created by caizj on 18-9-21.
  */
+@Mapper
 public interface LoginLogMapper {
+
+    int addLog(LoginLog loginLog);
+
+    int deleteLog(Long logid);
+
+    int deleteByUserid(String userid);
+
+    LoginLog getLog(Long logid);
+
+    List<LoginLog> queryGetLog(String userid);
 }
