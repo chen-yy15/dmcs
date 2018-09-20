5e57f0b784cae61de88be1fc38e456b13cce9377
caizj15 <caizj15@qq.com>
2018-09-20 12:12:58 +0800

    add UserFriend

---
+++ src/main/java/edu/tsinghua/dmcs/service/UserFriendService.java
@@ -0,0 +1,7 @@
+package edu.tsinghua.dmcs.service;
+
+/**
+ * Created by caizj on 18-9-20.
+ */
+public interface UserFriendService {
+}
---
+++ src/main/java/edu/tsinghua/dmcs/service/impl/UserFriendServiceImpl.java
@@ -0,0 +1,7 @@
+package edu.tsinghua.dmcs.service.impl;
+
+/**
+ * Created by caizj on 18-9-20.
+ */
+public class UserFriendServiceImpl {
+}
