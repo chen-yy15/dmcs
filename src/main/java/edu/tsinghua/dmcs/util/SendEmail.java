292709f104262ad78d39282ee2de6b3b0dfd08e5
caizj15 <caizj15@qq.com>
2018-09-23 01:00:18 +0800

    change SendEmail.java

--- src/main/java/edu/tsinghua/dmcs/util/SendEmail.java
+++ src/main/java/edu/tsinghua/dmcs/util/SendEmail.java
@@ -1,7 +1,25 @@
 package edu.tsinghua.dmcs.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by caizj on 18-9-22.
  */
+@Component
 public class SendEmail {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendSimEmail(String sendTo, String title, String content){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("caizj15@163.com");
+        message.setTo(sendTo);
+        message.setSubject(title);
+        message.setText(content);
+        javaMailSender.send(message);
+    }
 }
