b12b51aaf5dbc7ad40d81ee5968e8fbdd92f2589
caizj15 <caizj15@qq.com>
2018-10-26 10:45:22 +0800

    change entity

--- src/main/java/edu/tsinghua/dmcs/entity/AdminGroupUser.java
+++ src/main/java/edu/tsinghua/dmcs/entity/AdminGroupUser.java
@@ -4,26 +4,100 @@ package edu.tsinghua.dmcs.entity;
  * Created by caizj on 18-9-6.
  */
 public class AdminGroupUser {
+
     private String userid;
+
     private String avatar;
+
     private String userName;
+
     private String userEmail;
+
     private String userworkPlace;
+
     private String userTelephone;
-    private int authorityNumber;
+
+    private String auth1;
+
+    private String auth2;
+
+    private String auth3;
+
+    private String auth4;
+
+    private String auth5;
+
+    private String auth6;
+
+    private String auth7;
+
+    private String auth8;
+
+    private String auth9;
+
+    private String auth10;
 
     public String getUsername() {return userName;}
+
     public void setUsername(String userName) {this.userName=userName;}
+
     public String getUserid() {return userid;}
+
     public void setUserid(String userid) {this.userid=userid;}
+
     public String getAvatar() {return avatar;}
+
     public void setAvatar(String avatar) {this.avatar=avatar;}
+
     public String getUserEmail() {return userEmail;}
+
     public void setUserEmail(String userEmail) {this.userEmail=userEmail;}
+
     public String getUserworkPlace() {return userworkPlace;}
+
     public void setUserworkPlace(String userworkPlace) {this.userworkPlace=userworkPlace;}
+
     public String getUserTelephone() {return userTelephone;}
+
     public void setUserTelephone(String userTelephone) {this.userTelephone=userTelephone;}
-    public int getAuthorityNumber() {return authorityNumber;}
-    public void setAuthorityNumber(int authorityNumber) {this.authorityNumber = authorityNumber;}
+
+    public String getAuth1(){return auth1;}
+
+    public void setAuth1(String auth1) {this.auth1=auth1;}
+
+    public String getAuth2() {return auth2;}
+
+    public void setAuth2(String auth2) {this.auth2= auth2;}
+
+    public String getAuth3() {return auth3;}
+
+    public void setAuth3(String auth3) {this.auth3=auth3;}
+
+    public String getAuth4() {return auth4;}
+
+    public void setAuth4(String auth4) {this.auth4=auth4;}
+
+    public String getAuth5() {return auth5;}
+
+    public void setAuth5(String auth5) {this.auth5=auth5;}
+
+    public String getAuth6() {return auth6;}
+
+    public void setAuth6(String auth6) {this.auth6 = auth6;}
+
+    public String getAuth7() {return auth7;}
+
+    public void setAuth7(String auth7) {this.auth7=auth7;}
+
+    public String getAuth8() {return auth8;}
+
+    public void setAuth8(String auth8) {this.auth8=auth8;}
+
+    public String getAuth9() {return auth9;}
+
+    public void setAuth9(String auth9) {this.auth9=auth9;}
+
+    public String getAuth10() {return auth10;}
+
+    public void setAuth10(String auth10) {this.auth10= auth10;}
 }
