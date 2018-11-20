b89e65c5544d1eced2af5672006fb670f7f18fa3
caizj15 <caizj15@qq.com>
2018-11-20 13:32:24 +0800

    change service

--- src/main/java/edu/tsinghua/dmcs/service/EnumNameWebInfoService.java
+++ src/main/java/edu/tsinghua/dmcs/service/EnumNameWebInfoService.java
@@ -1,7 +1,28 @@
 package edu.tsinghua.dmcs.service;
 
+import edu.tsinghua.dmcs.entity.EnumName;
+import edu.tsinghua.dmcs.entity.WebInformation;
+
+import java.util.List;
+
 /**
  * Created by caizj on 18-11-8.
  */
 public interface EnumNameWebInfoService {
+
+    public int DeleteInfo(Long infid);
+
+    public int AddWebInfo(WebInformation webInformation);
+
+    public int UpdateWebInfo(WebInformation webInformation);
+
+    public WebInformation SelectWebInfo(Long infid);
+
+    public List<WebInformation> GetWebInfo();
+
+    public int UpdateNamedetail(EnumName enumName);
+
+    public EnumName GetEnumNameByid(Integer nameid);
+
+    public List<EnumName> GetFirPageEnums();
 }
--- src/main/java/edu/tsinghua/dmcs/service/impl/EnumNameWebInfoServiceImpl.java
+++ src/main/java/edu/tsinghua/dmcs/service/impl/EnumNameWebInfoServiceImpl.java
@@ -1,7 +1,69 @@
 package edu.tsinghua.dmcs.service.impl;
 
+import edu.tsinghua.dmcs.entity.EnumName;
+import edu.tsinghua.dmcs.entity.WebInformation;
+import edu.tsinghua.dmcs.mapper.EnumnameMapper;
+import edu.tsinghua.dmcs.mapper.WebInfoMapper;
+import edu.tsinghua.dmcs.service.EnumNameWebInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
 /**
  * Created by caizj on 18-11-8.
  */
-public class EnumNameWebInfoServiceImpl {
+@Component
+public class EnumNameWebInfoServiceImpl implements EnumNameWebInfoService {
+
+    @Autowired
+    WebInfoMapper webInfoMapper;
+
+    @Autowired
+    EnumnameMapper enumnameMapper;
+
+    public int DeleteInfo(Long infid){
+        int num = 0;
+        num = webInfoMapper.deleteById(infid);
+        return num;
+    }
+
+    public int AddWebInfo(WebInformation webInformation){
+        int num = 0;
+        if(webInformation!=null){
+            num = webInfoMapper.addWebInfo(webInformation);
+        }
+        return num;
+    }
+
+    public int UpdateWebInfo(WebInformation webInformation){
+        int num = 0;
+        if(webInformation!=null) {
+            num = webInfoMapper.updateWebInfo(webInformation);
+        }
+        return num;
+    }
+
+    public WebInformation SelectWebInfo(Long infid){
+        return webInfoMapper.selectById(infid);
+    }
+
+    public List<WebInformation> GetWebInfo(){
+        return webInfoMapper.getWebInfo();
+    }
+
+    public int UpdateNamedetail(EnumName enumName){
+        int num = 0;
+        if(enumName!=null){
+           num = enumnameMapper.UpdateNamedetail(enumName);
+        }
+        return num;
+    }
+
+    public EnumName GetEnumNameByid(Integer nameid){
+        return enumnameMapper.getEnumNameByid(nameid);
+    }
+    public List<EnumName> GetFirPageEnums(){
+        return enumnameMapper.GetFirPageEnums();
+    }
 }
