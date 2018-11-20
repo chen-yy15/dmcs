7619bf561ca7c33b29f7044a4619e77eae057164
caizj15 <caizj15@qq.com>
2018-11-20 13:31:58 +0800

    change Enummapper

--- src/main/java/edu/tsinghua/dmcs/mapper/EnumnameMapper.java
+++ src/main/java/edu/tsinghua/dmcs/mapper/EnumnameMapper.java
@@ -1,7 +1,19 @@
 package edu.tsinghua.dmcs.mapper;
 
+import edu.tsinghua.dmcs.entity.EnumName;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
 /**
  * Created by caizj on 18-11-8.
  */
+@Mapper
 public interface EnumnameMapper {
+
+    EnumName getEnumNameByid (Integer nameid);
+
+    int UpdateNamedetail (EnumName enumName);
+
+    List<EnumName> GetFirPageEnums();
 }
